ee2b466eeeb56a6bdfaac567c89e076a4da58f3f
Austin Blackman <austin.blackman@gmail.com>
2020-12-27 21:58:40 -0500

    removing public url from resume link

--- src/components/contact_info/ContactInfo.jsx
+++ src/components/contact_info/ContactInfo.jsx
@@ -8,7 +8,7 @@ export default function ContactInfo(){
       <h1>Austin Blackman</h1>
       <ul>
         <li>
-          <a href={process.env.PUBLIC_URL+'/austin_blackman_resume.pdf'}>
+          <a href={'/austin_blackman_resume.pdf'}>
             resume {''}<i className="fas fa-file-pdf"></i>
           </a>
         </li>
